800d871bba7405b2e023fcc03bf874939068ad07
demogorgon1 <neckelmann@gmail.com>
2025-07-20 22:32:48 +0200

    - Fixed tests.

--- lib/fs_mem.h
+++ lib/fs_mem.h
@@ -1,6 +1,8 @@
 #ifndef __FS_MEM_H__
 #define __FS_MEM_H__
 
+#include <stddef.h>
+
 #define FS_NEW(_Type) (_Type*)fs_mem_zalloc(sizeof(_Type))
 #define FS_NEW_ARRAY(_Type, _Count) (_Type*)fs_mem_zalloc(sizeof(_Type) * _Count)
 
